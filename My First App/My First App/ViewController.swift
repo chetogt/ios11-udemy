bfa97e7be00d030a60a0fd7a5dcf1768c18119f8
José Córdova <jacp@jumay.biz>
2018-05-31 16:47:42 -0700

    Added folder for section 1: My First App

---
+++ My First App/My First App/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  My First App
+//
+//  Created by Jose on 5/23/18.
+//  Copyright © 2018 Plicod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello World from Swift 4!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
